34cd6d006b4c3d561c1f91ff13b7d5d5f1e6ab96
Loopfelloff <abhiyanregmi@gmail.com>
2024-12-16 05:30:02 +0545

    added user and owner verification with backend

--- database/user_database.sql
+++ database/user_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2024 at 04:00 PM
+-- Generation Time: Dec 16, 2024 at 12:40 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -39,8 +39,38 @@ CREATE TABLE `signin` (
 --
 
 INSERT INTO `signin` (`email`, `firstName`, `lastName`, `password`) VALUES
+('abhiyanregmi12@gmail.com', 'Abhiyandon', 'Regmi', '$2y$10$rv3iKh13ZDabghMrEHLxh.Vn7km/Q4wCL4fR2D0L6VwCCswcmoQSu'),
 ('abhiyanthapa@gmail.com', 'abhiyan ', 'thapa', '$2y$10$L9WjpF6ildsVby2IKfEkMu1e7dAka3bXXDawsiyq2NfqmedKm/gTu'),
-('swastikbhandari2006@gmail.com', 'swastik ', 'bhandari', '$2y$10$zEUbk9kpMcUPswB4PUkLeuwdnnMreLam8Yo.TRp5Rf4D9U/Ky1POu');
+('qwertyuiop@gmail.com', 'yesboi', 'hello', '$2y$10$3T1jU4zmWzlmWvGifqFWiONJyxA/GmgHhN47hoq7v2Ei473U.JA0.'),
+('swastikbhandari2006@gmail.com', 'swastik ', 'bhandari', '$2y$10$zEUbk9kpMcUPswB4PUkLeuwdnnMreLam8Yo.TRp5Rf4D9U/Ky1POu'),
+('university@gmail.com', 'yeah boi', 'regmi', '$2y$10$aU7OpjrQ4cWRJaX1WFle0eSSw1SWq3bj0eqA1TPDmNNSNz67sH5ZG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_verification`
+--
+
+CREATE TABLE `user_verification` (
+  `email` varchar(255) NOT NULL,
+  `verification_number` int(255) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_verification`
+--
+
+INSERT INTO `user_verification` (`email`, `verification_number`, `status`) VALUES
+('234567@gmail.com', 3456, 'student'),
+('3456765432ertytrew@gmail.com', 6543, 'owner'),
+('abhiaynregmi12@gmail.com', 23456, 'owner'),
+('abhiyanregmi12@gmail.com', 2147483647, 'student'),
+('abhiyanregmi@gmail.com', 23456, 'student'),
+('abhiyanthapa@gmail.com', 2345678, 'student'),
+('fjfsdfalsdjfals@gmail.com', 2147483647, 'student'),
+('swastikbhandari2006@gmail.com', 2006, 'student'),
+('university@gmail.com', 123456, 'student');
 
 --
 -- Indexes for dumped tables
@@ -50,6 +80,12 @@ INSERT INTO `signin` (`email`, `firstName`, `lastName`, `password`) VALUES
 -- Indexes for table `signin`
 --
 ALTER TABLE `signin`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `user_verification`
+--
+ALTER TABLE `user_verification`
   ADD PRIMARY KEY (`email`);
 COMMIT;
 
