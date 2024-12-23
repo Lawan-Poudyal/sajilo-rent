8ba5e77690631a1d58865b6ada5cac655a98bc02
Lawan-Poudyal <poudyallawan@gmail.com>
2024-12-24 03:57:11 +0545

    Added room info container

---
+++ student-scroll-section/scripts/data/rooms-info.js
@@ -0,0 +1,15 @@
+export const rooms = [
+    {
+        id: '1',
+        image: "../../../resources/rooms/room1.jpg"
+    },
+    {
+        id: '2',
+        image: "../../../resources/rooms/room2.jpg"
+    },
+    {
+        id: '3',
+        image: "../../../resources/rooms/room3.jpg"
+    },
+
+]
