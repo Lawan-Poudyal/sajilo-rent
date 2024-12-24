e7b39a0a659610da627937643fef40ae54c6e95d
Lawan-Poudyal <poudyallawan@gmail.com>
2024-12-24 10:11:44 +0545

    room info array creation

--- student-scroll-section/scripts/data/rooms-info.js
+++ student-scroll-section/scripts/data/rooms-info.js
@@ -1,15 +1,33 @@
 export const rooms = [
     {
         id: '1',
-        image: "../../../resources/rooms/room1.jpg"
+        image: "../../../resources/rooms/room1.jpg",
+        name: "Single Room",
+        rating: {
+            stars: 4.5,
+            count: 87,
+        },
+        price: 3000
     },
     {
         id: '2',
-        image: "../../../resources/rooms/room2.jpg"
+        image: "../../../resources/rooms/room2.jpg",
+        name: "Double Room",
+        rating: {
+            stars: 2.5,
+            count: 87,
+        },
+        price: 4000
     },
     {
         id: '3',
-        image: "../../../resources/rooms/room3.jpg"
+        image: "../../../resources/rooms/room3.jpg",
+        name: "Apartment sunny ku Room",
+        rating: {
+            stars: 3.5,
+            count: 87,
+        },
+        price: 5000
     },
 
 ]
