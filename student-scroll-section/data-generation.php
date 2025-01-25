33aac493577a30286eb84c27eb9747cfc75a1333
Lawan-Poudyal <poudyallawan@gmail.com>
2025-01-25 21:36:42 +0545

    ...

--- student-scroll-section/data-generation.php
+++ student-scroll-section/data-generation.php
@@ -6,25 +6,25 @@ include("db.php");
 
 $stmt = $conn->prepare("SELECT * from housedetails ");
 if (!$stmt) {
-    die('Query preparation failed: ' . $conn->error);
+  die('Query preparation failed: ' . $conn->error);
 }
 // Execute the statement
 $stmt->execute();
 if (!$stmt->execute()) {
-    die('Execution failed: ' . $stmt->error);
+  die('Execution failed: ' . $stmt->error);
 }
 
 // fetch result set
 $result = $stmt->get_result();
 if (!$result) {
-    die("Fetching results faild: " . $stmt->error);
+  die("Fetching results faild: " . $stmt->error);
 }
 
 // Fetch datas as an associative array 
 
 $rooms = [];
 while ($row = $result->fetch_assoc()) {
-    $rooms[] = $row;
+  $rooms[] = $row;
 }
 
 
@@ -37,10 +37,10 @@ $roomsHTML = '';
 
 
 if (!empty($rooms)) {
-    foreach ($rooms as $room) {
+  foreach ($rooms as $room) {
 
 
-        $roomsHTML .=   ' <div class="products-container">
+    $roomsHTML .=   ' <div class="room-container">
                 <div class="products-image-container">
                   <img
                     src="' . htmlspecialchars($room['image1']) . '"
@@ -59,16 +59,14 @@ if (!empty($rooms)) {
                 <div class="product-price">Rs ' . htmlspecialchars($room['price']) . '</div>
 
                 <div class="product-spacer"></div>
-                <div class="added-to-cart">
-                  <img src="../images/icons/checkmark.png" />
-                  Added
-                </div>
-                <a href="#">
-                <button class="add-to-cart-button button-primary " data-product-id=${room.id
-          }>Location</button>
-          </a>
-              </div>';
-    }
+              
+                
+                <button class="location-button button-primary " >Location</button>
+                <button class="details-button button-primary " >Details</button>
+                 
+              </div>
+             ';
+  }
 } else {
-    echo "<p>No Rooms found.</p>";
+  echo "<p>No Rooms found.</p>";
 }
