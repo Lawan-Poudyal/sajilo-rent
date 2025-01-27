26f9ab980f9aef63545c597450cf9b44c4303cc5
Lawan-Poudyal <poudyallawan@gmail.com>
2025-01-27 22:02:16 +0545

    backend and frontend communication to display popup in map

--- studentsection/script/location.js
+++ studentsection/script/location.js
@@ -1,27 +1,32 @@
-import { Map, RoutingControl, MarkerMaker, SelectRanges } from './classes.js';
+const locationButton = document.querySelectorAll('.js-location-button');
 
-// const locationButton = document.querySelector('.js-location-button');
+locationButton.forEach((button)=>{
 
-// locationButton.addEventListener("click", (event)=>{
-   
-//     console.log(event);
-// });
+    button.addEventListener("click", ()=>{
 
+         const lat = button.getAttribute('data-room-lat');
+         const lng = button.getAttribute('data-room-lng');
 
-const center = [27.620339825608795, 85.5381077528];
-const zoom = 20;
-const tileLayer = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+        // Save the location in the backend
+        fetch('/sajilo-rent/studentsection/backend/saveLocation.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ lat: parseFloat(lat), lng: parseFloat(lng) })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'success') {
+                // Redirect to the map page
+                window.location.href = '/sajilo-rent/studentsection/displayLatLng.php';
+            } else {
+                console.error('Error:', data.message);
+            }
+        })
+        .catch(error => console.error('Error:', error));
 
-const map = new Map(center, zoom, tileLayer, attribution);
-const mapInstance = map.createMap();
+            
+    });
 
-let markerMaker, routingLocation;
-
-markerMaker = new MarkerMaker(mapInstance);
-
-routingLocation = new RoutingControl(mapInstance, markerMaker, center);
-const lat = 27.61970291051658;
-const lng = 85.53646624088287;
-
-routingLocation.addRoutingControl(lat, lng);
+});
