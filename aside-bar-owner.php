c4bfbb3c49ef58a6527cb478042efc8d6291962c
Loopfelloff <abhiyanregmi@gmail.com>
2025-03-03 20:13:27 +0545

    some error here

--- aside-bar-owner.php
+++ aside-bar-owner.php
@@ -9,7 +9,7 @@ $email = $_SESSION['email'];
                 <a href="/sajilo-rent/chatapplication/messenger.php?email=<?php echo $email?>"><img src="https://img.icons8.com/ios/50/messages-mac.png" alt="messages-icon"/><span class="link-text messages">Messages</span></a>
                 <a href="#"><img src="https://img.icons8.com/ios/50/conference-call.png" alt="tenants profile icon"/><span class="link-text tenants-proile">Tenants Profile</span></a>
 <!----create the tenants request section by yourselves ----->
-                <a href=""><img src="https://img.icons8.com/fluency-systems-regular/50/request-feedback.png" alt="rent request icon"/><span class="link-text rent-request">Rent Request</span></a>
+                <a href="" class="js-tenants-request"><img src="https://img.icons8.com/fluency-systems-regular/50/request-feedback.png" alt="rent request icon"/><span class="link-text rent-request">Rent Request</span></a>
                 <a href="#"><img src="https://img.icons8.com/fluency-systems-regular/50/password-window.png" alt="password icon"/><span class="link-text change-password">Change Password</span></a>
             </div>
             <div class="bottom-links">
@@ -157,6 +157,7 @@ async function callChangePassword(changePasswordData){
         </div>
     </form>
 </dialog>
+
 <!-- script for logout -->
 
 <script>
@@ -183,5 +184,17 @@ cancelLogoutIcon.addEventListener('click', () => {
 cancelLogoutButton.addEventListener('click', () => {
 
     logoutDialog.close();
+});
+</script>
+<!------------------ dialog for seeing tenants profile dumbo  --->
+<diaglog class="main-section-div request js-request-card xyz ">
+        
+</dialog>
+<!----------this is the script to load the tenants profile dumbo -->
+<script>
+const tenantsRequest = document.querySelector('.js-tenants-request');
+const requestCard  = document.querySelector('.js-request-card');
+tenantsRequest.addEventListener('click' , ()=>{
+requestCard.showModal();
 });
 </script>
